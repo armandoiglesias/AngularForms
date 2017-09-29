20473b3df270c56fd255e7de3836bd13d5eb18f1
Armando <iglesias.armando@gmail.com>
2017-09-29 00:20:50 -0300

    first commit

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm} from '@angular/forms'
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: [
+    `
+      .ng-invalid.ng-touched:not(form) {
+        border-color : red;
+      }
+    `
+  ]
+})
+export class TemplateComponent implements OnInit {
+
+  usuario:Object = {
+    nombre : null
+    , apellido : null
+    , correo : null
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  guardar(forma:NgForm){
+    console.log("Posteado");
+    console.log("ngForm :" + forma);
+    console.log("Valor" , forma.value)
+
+    console.log("usuario", this.usuario);
+  }
+
+}
